12b58c77e1e63a2176f05ed51f56774e90f17571
Vishaal Kalwani <vishaalk@umich.edu>
2016-03-27 15:01:50 -0700

    Converted lastButOne to use a Maybe
    ;

--- ch2/lastButOne.hs
+++ ch2/lastButOne.hs
@@ -1,3 +1,4 @@
+lastButOne :: [a] -> Maybe a
 lastButOne xs = if length xs <= 1
-                then -1 
-                else (head (drop (length xs - 2) xs))
+                then Nothing
+                else Just (head (drop (length xs - 2) xs))
